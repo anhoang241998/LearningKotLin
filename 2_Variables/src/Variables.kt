7cb1718b3e9aedd1ed6064c5aae752a7871a72d6
HoangAn <an2419998@gmail.com>
2020-06-16 13:49:34 +0700

    finish lesson 2

---
+++ 2_Variables/src/Variables.kt
@@ -0,0 +1,20 @@
+fun main() {
+    // mutable, immutable
+
+    //mutable: khong the gan lai gia tri
+    var email: String = "code4func@gmail.com"
+    email = "an2419998@gmail.com"
+    println(email)
+
+    //immutable: khong the gan lai gia tri
+    val name: String = "Ryan Nguyen"
+//    name = "Nguyen Hoang An"
+    print(name)
+
+    /*Luu y 1: khi khoi tao bien phai gan gia tri vo
+    Luu y 2: trong kotlin khong nhat thiet khai bao
+    tuong minh mot kieu du lieu nao*/
+//  Example
+    var myName  =  "Nguyen Hoang An"
+    print(myName)
+}
